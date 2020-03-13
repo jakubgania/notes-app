563f0a0c3092c6c01651e94fe5d8c1fed726bbf7
jakubgania <contact@jakubgania.io>
2020-03-13 19:07:23 +0100

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
-var notes = require('./routes/notes');
+var notesRouter = require('./routes/notes');
 
 var app = express();
 
@@ -22,12 +22,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
-app.use('/notes', notes);
+app.use('/notes', notesRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,9 @@
-'use strict';
+// 'use strict';
 
-var util = require('util');
+// var util = require('util');
 var express = require('express');
 var router = express.Router();
-var notes = require('../models/notes-memory');
+// var notes = require('../models/notes-memory');
 
 router.get('/add', (req, res, next) => {
   res.render('noteedit', {
@@ -12,6 +12,19 @@ router.get('/add', (req, res, next) => {
     notekey: "",
     note: undefined
   });
+});
+
+router.post('/save', (req, res, next) => {
+  var p;
+  if (req.body.docreate === "create") {
+    p = notes.docreate(req.body.notekey, req.body.title, req.body.body);
+  } else {
+    p = notes.update(req.body.notekey, req.body.title, req.body.body);
+  }
+  p.then(note => {
+    res.redirect('/notes/view?key=' + req.body.notekey);
+  })
+  .catch(err => { next(err) });
 });
 
 module.exports = router;
